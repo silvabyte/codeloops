e7619d4e6d5c1f30c3de3a7b0e8288b8986ebede
Mat Silva <matsilva@hey.com>
2026-01-28 14:49:31 -0500

    feat: stylize the docs site

--- docs/book/searcher-c2a407aa.js
+++ docs/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-29d01579.js',
+                path_to_root + 'searchindex-e5ba35e3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
